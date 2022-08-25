701886c1d14732913df4a5d50aaba0017be27769
Vadim Savin <savinvadim1312@gmail.com>
2022-08-25 16:58:42 +0100

    8. Image Picker

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { View, Text, StyleSheet, TextInput, Image, Button } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 const user = {
   id: "u1",
@@ -10,10 +12,26 @@ const user = {
 
 const CreatePostScreen = () => {
   const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
 
   const onPost = () => {
     console.warn("Posting: ", description);
     setDescription("");
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
   };
 
   return (
@@ -23,6 +41,13 @@ const CreatePostScreen = () => {
       <View style={styles.header}>
         <Image source={{ uri: user.image }} style={styles.profileImage} />
         <Text style={styles.name}>{user.name}</Text>
+        <Entypo
+          onPress={pickImage}
+          name="images"
+          size={24}
+          color="limegreen"
+          style={styles.icon}
+        />
       </View>
 
       <TextInput
@@ -32,8 +57,11 @@ const CreatePostScreen = () => {
         style={styles.input}
         multiline
       />
+      <Image source={{ uri: image }} style={styles.image} />
 
-      <Button onPress={onPost} title="Post" disabled={!description} />
+      <View style={styles.buttonContainer}>
+        <Button onPress={onPost} title="Post" disabled={!description} />
+      </View>
     </View>
   );
 };
@@ -61,8 +89,16 @@ const styles = StyleSheet.create({
   name: {
     fontWeight: "500",
   },
-  input: {
-    marginBottom: "auto",
+  input: {},
+  buttonContainer: {
+    marginTop: "auto",
+  },
+  icon: {
+    marginLeft: "auto",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 4 / 3,
   },
 });
 
