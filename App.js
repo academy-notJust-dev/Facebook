f0ab5e47377b5bd8517f2b38fe63142c20ef70dd
Vadim Savin <savinvadim1312@gmail.com>
2022-08-25 16:06:17 +0100

    7. Create Post Screen

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import FeedScreen from "./src/screens/FeedScreen";
+import CreatePostScreen from "./src/screens/CreatePostScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <FeedScreen />
+      <CreatePostScreen />
     </View>
   );
 }
---
+++ src/screens/CreatePostScreen.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Image, Button } from "react-native";
+
+const user = {
+  id: "u1",
+  image:
+    "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg",
+  name: "Vadim Savin",
+};
+
+const CreatePostScreen = () => {
+  const [description, setDescription] = useState("");
+
+  const onPost = () => {
+    console.warn("Posting: ", description);
+    setDescription("");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Create Post Screen</Text>
+
+      <View style={styles.header}>
+        <Image source={{ uri: user.image }} style={styles.profileImage} />
+        <Text style={styles.name}>{user.name}</Text>
+      </View>
+
+      <TextInput
+        placeholder="What's on your mind?"
+        value={description}
+        onChangeText={setDescription}
+        style={styles.input}
+        multiline
+      />
+
+      <Button onPress={onPost} title="Post" disabled={!description} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    width: "100%",
+    padding: 10,
+    paddingTop: 30,
+  },
+  header: {
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: "500",
+  },
+  input: {
+    marginBottom: "auto",
+  },
+});
+
+export default CreatePostScreen;
