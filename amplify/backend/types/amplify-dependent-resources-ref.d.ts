be1f239883662130c4b495be5eb72ee91c6a4ca6
Vadim Savin <savinvadim1312@gmail.com>
2022-08-26 19:36:01 +0100

    BE-3 Deploy to AWS

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "SocialMediaPrep": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
