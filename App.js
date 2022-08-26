b8e6c282fb207f42480b7dc4b16753b2143de664
Vadim Savin <savinvadim1312@gmail.com>
2022-08-26 19:59:51 +0100

    BE-4 Authentication

--- App.js
+++ App.js
@@ -2,12 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import Navigator from "./src/navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth } from "aws-amplify";
 import awsconfig from "./src/aws-exports";
+import { withAuthenticator } from "aws-amplify-react-native";
+import { useEffect, useState } from "react";
 
-Amplify.configure(awsconfig);
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
-export default function App() {
+function App() {
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    Auth.currentAuthenticatedUser().then(setAuthUser);
+  }, []);
+
+  console.log(authUser);
+
   return (
     <SafeAreaProvider style={styles.container}>
       <StatusBar style="auto" />
@@ -23,3 +33,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#c9c9c9",
   },
 });
+
+export default withAuthenticator(App);
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "SocialMediaPrep": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
 }
